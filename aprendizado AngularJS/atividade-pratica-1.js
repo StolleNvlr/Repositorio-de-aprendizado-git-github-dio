efe12a49abaa4fb24ee13369b3ce772309342e66
StolleN <biel09gomes@gmail.com>
2023-04-04 23:23:28 -0300

    correção de problemas no codigo

--- aprendizado AngularJS/atividade-pratica-1.js
+++ aprendizado AngularJS/atividade-pratica-1.js
@@ -1,13 +1,13 @@
-const nota1 = 10;
-const nota2 = 10;
+const nota1 = 5;
+const nota2 = 4;
 const nota3 = 6;
 
 const Media = (nota1 + nota2 + nota3) / 3;
 
 if (Media >= 7) {
     console.log('Aprovado com media:', Media.toFixed(2))
-} else if (Media >=5) {
-    console.log('Recuperação com media:',Media.toFixed(2))
-} else if (Media <=5) {
+} else if (Media >= 5) {
+    console.log('Recuperação com media:', Media.toFixed(2))
+} else if (Media < 5) {
     console.log('Reprovado com media:', Media.toFixed(2))
 }

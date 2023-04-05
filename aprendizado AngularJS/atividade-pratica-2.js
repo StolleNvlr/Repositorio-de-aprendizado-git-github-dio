a92c9fb7092d43a515d1e7b07ae3623d7a7763f2
StolleN <biel09gomes@gmail.com>
2023-04-04 23:50:25 -0300

    adicionado atividade pratica pt 2

---
+++ aprendizado AngularJS/atividade-pratica-2.js
@@ -0,0 +1,17 @@
+const peso = 200;
+const altura = 1.79
+const IMC = peso / (altura**2);
+
+console.log(IMC.toFixed(2))
+
+if(IMC < 18.5) {
+    console.log('Abaixo do peso')
+} else if (IMC >= 18.5 && IMC < 25) {
+    console.log('Peso Normal')
+} else if (IMC >= 25 && IMC < 30) {
+    console.log('Acima do peso')
+} else if (IMC >= 30 && IMC < 40) {
+    console.log('Obeso')
+} else if (IMC > 40) {
+    console.log('Obesidade morbida')
+}
